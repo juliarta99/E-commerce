8f412e67f99d0f483402ce14fa7d5462b5788294
juliarta <111062349+juliarta99@users.noreply.github.com>
2023-01-21 09:02:08 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="w-full flex items-center justify-cente h-screen">
+        <h1 class="text-blue-500 text-xl text-center w-full">Halo Ini Setup Web E-commerce</h1>
+    </div>
+</body>
+</html>
