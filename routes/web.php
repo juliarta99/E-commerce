38296392739bb7d79669a1a2879f511a3f88dc73
juliarta99 <adij4255@gmail.com>
2023-10-09 16:02:40 +0800

    refactor: code routes

--- routes/web.php
+++ routes/web.php
@@ -25,58 +25,57 @@ use App\Http\Controllers\VerificationController;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/products', [ProductController::class, 'index'])->name('home.products');
+Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name('home.products.search');
 
-// login
-Route::get('/login', [AuthController::class, 'index'])->name('login')->middleware('guest');
-Route::post('/login', [AuthController::class, 'authenticate'])->name('login')->middleware('guest');
-Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'index'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticate'])->name('login.create');
+    
+    Route::get('/register', [AuthController::class, 'create'])->name('register');
+    Route::post('/register', [AuthController::class, 'store'])->name('register.create');
+});
 
-Route::get('/register', [AuthController::class, 'create'])->middleware('guest');
-Route::post('/register', [AuthController::class, 'store'])->middleware('guest');
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/editProfile', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('/editProfile', [UserController::class, 'update'])->name('user.update');
+    
+    Route::get('/alamat', [AlamatController::class, 'index'])->name('alamat');
+    Route::get('/alamat/tambah', [AlamatController::class, 'create'])->name('alamat.create');
+    Route::post('/alamat', [AlamatController::class, 'store'])->name('alamat.store');
+    Route::get('/alamat/{alamat:id}/edit', [AlamatController::class, 'edit'])->name('alamat.edit');
+    Route::put('/alamat/{alamat:id}', [AlamatController::class, 'update'])->name('alamat.update');
+    Route::delete('/alamat/{alamat:id}', [AlamatController::class, 'destroy'])->name('alamat.delete');
+    
+    Route::get('/keranjang', [KeranjangController::class, 'index'])->name('keranjang');
+    Route::post('/keranjang/create', [KeranjangController::class, 'store'])->name('keranjang.create');
+    Route::put('/keranjang/update', [KeranjangController::class, 'update'])->name('keranjang.update');
+    Route::delete('/keranjang/delete', [KeranjangController::class, 'destroy'])->name('keranjang.delete');
+    
+    Route::get('/favorit', [FavoritController::class, 'index'])->name('favorit');
+    Route::post('/favorit/create', [FavoritController::class, 'store'])->name('favorit.create');
+    Route::delete('/favorit/delete', [FavoritController::class, 'destroy'])->name('favorit.delete');
+    
+    Route::get('/{toko:slug}', [TokoController::class, 'show'])->name('toko.show');
+    Route::get('/toko/create', [TokoController::class, 'create'])->name('toko.create');
+    Route::post('/toko/create', [TokoController::class, 'store'])->name('toko.store');
+});
+
+Route::middleware('is_admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
+
+Route::middleware('is_toko')->group(function () {
+    Route::get('/toko', [TokoController::class, 'index'])->name('toko');
+    Route::get('/toko/{toko:slug}/edit', [TokoController::class, 'edit'])->name('toko.edit');
+    Route::put('/toko/{toko:slug}', [TokoController::class, 'update'])->name('toko.update');
+    Route::get('/toko/{toko:slug}/editBack', [TokoController::class, 'editBack'])->name('toko.editBack');
+    Route::put('/toko/{toko:slug}/editBack', [TokoController::class, 'updateBack'])->name('toko.updateBack');
+    Route::resource('/toko/product', TokoProductController::class);
+});
 
 // verification akun
-// Route::get('/email/verify', [VerificationController::class, 'notice'])->middleware('auth')->name('verification.notice');
+// Route::get('/email/verify', [VerificationController::class, 'notice'])->name('verification.notice');
 // Route::get('/email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->middleware('auth', 'signed')->name('verification.verify');
 // Route::post('/email/resend-verify', [VerificationController::class, 'resend'])->middleware('auth', 'throttle:6,1')->name('verification.send');
-
-// product
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/product/{product:slug}', [ProductController::class, 'show']);
-
-// profile
-Route::get('/editProfile', [UserController::class, 'edit'])->middleware('auth');
-Route::put('/editProfile', [UserController::class, 'update'])->middleware('auth');
-
-// alamat
-Route::get('/alamat', [AlamatController::class, 'index'])->middleware('auth');
-Route::get('/alamat/tambah', [AlamatController::class, 'create'])->middleware('auth');
-Route::post('/alamat', [AlamatController::class, 'store'])->middleware('auth');
-Route::get('/alamat/{alamat:id}/edit', [AlamatController::class, 'edit'])->middleware('auth');
-Route::put('/alamat/{alamat:id}', [AlamatController::class, 'update'])->middleware('auth');
-Route::delete('/alamat/{alamat:id}', [AlamatController::class, 'destroy'])->middleware('auth');
-
-// keranjang
-Route::post('/keranjang/create', [KeranjangController::class, 'store'])->middleware('auth');
-Route::get('/keranjang', [KeranjangController::class, 'index'])->middleware('auth');
-Route::put('/keranjang/update', [KeranjangController::class, 'update'])->middleware('auth');
-Route::delete('/keranjang/delete', [KeranjangController::class, 'destroy'])->middleware('auth');
-
-// favorit
-Route::get('/favorit', [FavoritController::class, 'index'])->middleware('auth');
-Route::post('/favorit/create', [FavoritController::class, 'store'])->middleware('auth');
-Route::delete('/favorit/delete', [FavoritController::class, 'destroy'])->middleware('auth');
-
-// admin
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('is_admin')->name('dashboard');
-
-
-// toko
-Route::get('/toko', [TokoController::class, 'index'])->middleware('is_toko');
-Route::get('/toko/create', [TokoController::class, 'create'])->middleware('auth');
-Route::post('/toko/create', [TokoController::class, 'store'])->middleware('auth');
-Route::get('/toko/{toko:slug}/edit', [TokoController::class, 'edit'])->middleware('is_toko');
-Route::put('/toko/{toko:slug}', [TokoController::class, 'update'])->middleware('is_toko');
-Route::get('/toko/{toko:slug}/editBack', [TokoController::class, 'editBack'])->middleware('is_toko');
-Route::put('/toko/{toko:slug}/editBack', [TokoController::class, 'updateBack'])->middleware('is_toko');
-Route::get('/{toko:slug}', [TokoController::class, 'show'])->middleware('auth');
-Route::resource('/toko/product', TokoProductController::class)->middleware('is_toko');
